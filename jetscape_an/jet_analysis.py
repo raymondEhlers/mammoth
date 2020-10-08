67cc6e436c8dda1babdfa7dfb504b552b993c6e3
Raymond Ehlers <raymond.ehlers@gmail.com>
2020-10-08 22:54:17 +0200

    Add some QA plots, general improvements
    Still some checks to be done

--- jetscape_an/jet_analysis.py
+++ jetscape_an/jet_analysis.py
@@ -4,13 +4,29 @@
 """
 
 import functools
-from typing import Any, Mapping, Type, TypeVar
+from pathlib import Path
+from typing import Any, Mapping, Tuple, Type, TypeVar
 
 import awkward1 as ak
+import boost_histogram as bh
+import matplotlib
+import matplotlib.pyplot as plt
 import numba as nb
 import numpy as np
+import pachyderm.plot
 import particle
 import pyfastjet as fj
+from pachyderm import binned_data
+
+
+pachyderm.plot.configure()
+# Enable ticks on all sides
+# Unfortunately, some of this is overriding the pachyderm plotting style.
+# That will have to be updated eventually...
+matplotlib.rcParams["xtick.top"] = True
+matplotlib.rcParams["xtick.minor.top"] = True
+matplotlib.rcParams["ytick.right"] = True
+matplotlib.rcParams["ytick.minor.right"] = True
 
 
 class LorentzVectorCommon:
@@ -167,7 +183,7 @@ def find_jets_arr(array: ak.Array) -> ak.Array:
     #array["m"] = builder.snapshot()
     mass = builder.snapshot()
 
-    new_array = LorentzVectorArray.from_ptetaphim(new_array["pt"], new_array["eta"], new_array["phi"], mass)
+    new_array_lorentz = LorentzVectorArray.from_ptetaphim(new_array["pt"], new_array["eta"], new_array["phi"], mass)
 
     jet_defintion = fj.JetDefinition(fj.JetAlgorithm.antikt_algorithm, R = 0.4)
     area_definition = fj.AreaDefinition(fj.AreaType.passive_area, fj.GhostedAreaSpec(1, 1, 0.05))
@@ -177,12 +193,15 @@ def find_jets_arr(array: ak.Array) -> ak.Array:
     # TODO: Handle this more gracfully...
     temp_array = ak.zip(
         {
-            "E": new_array["t"],
-            "px": new_array["x"],
-            "py": new_array["y"],
-            "pz": new_array["z"],
+            "E": new_array_lorentz["t"],
+            "px": new_array_lorentz["x"],
+            "py": new_array_lorentz["y"],
+            "pz": new_array_lorentz["z"],
+            # NOTE: Having status is okay, even though it's not part of the LorentzVector. Which is quite nice!
+            "status": new_array["status"],
         },
-        #with_name="LorentzVector",
+        # TODO: This isn't quite right, but fine for now.
+        with_name="LorentzVector",
     )
     print(temp_array.type)
 
@@ -194,29 +213,175 @@ def find_jets_arr(array: ak.Array) -> ak.Array:
     #jets = ak.Array(fj.find_jets_awkward_test(events=temp_array.layout))
 
     jets = fj.find_jets(events=temp_array, settings=settings)
-    import IPython; IPython.embed()
+    #import IPython; IPython.embed()
+    return jets
 
+
+def particle_pt_by_status(arrays: ak.Array, pt_hat_bin: Tuple[int, int], base_output_dir: Path) -> None:
+    """ Plot particle pt by status code.
+
+    """
+    # Setup
+    output_dir = base_output_dir
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Particle selection
+    # Drop neutrinos.
+    arrays = arrays[(np.abs(arrays["particle_ID"]) != 12) & (np.abs(arrays["particle_ID"]) != 14) & (np.abs(arrays["particle_ID"]) != 16)]
+
+    all_status_codes = np.unique(ak.to_numpy(ak.flatten(arrays["status"])))
+    n_total_particles = ak.sum(ak.num(arrays, axis=1))
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    for status_code in all_status_codes:
+        print(f"status_code: {status_code}")
+        mask = (arrays["status"] == status_code)
+        pt_hist = bh.Histogram(bh.axis.Regular(100, 0, 50), storage=bh.storage.Weight())
+        pt_hist.fill(ak.to_numpy(ak.flatten(arrays[mask]["pt"])))
+        hist = binned_data.BinnedData.from_existing_data(pt_hist)
+
+        fraction_of_particles = ak.sum(ak.num(arrays[mask], axis=1)) / n_total_particles
+        print(f"Fraction of particles: {fraction_of_particles:.02g}")
+
+        # Normalize
+        hist /= hist.axes[0].bin_widths
+
+        ax.errorbar(
+            hist.axes[0].bin_centers,
+            hist.values,
+            xerr=hist.axes[0].bin_widths / 2,
+            label=f"Status = {status_code}, {int(fraction_of_particles * 100)}\%",
+            marker="o",
+            linestyle="",
+        )
+
+    # Label
+    ax.text(
+        0.45,
+        0.97,
+        r"$\hat{p_{\text{T}}} =$ " + f"{pt_hat_bin[0]}-{pt_hat_bin[1]}",
+        transform=ax.transAxes,
+        horizontalalignment="left", verticalalignment="top", multialignment="left",
+    )
+    ax.set_yscale("log")
+    ax.set_ylabel(r"dN/d$p_{\text{T}}$ $(\text{GeV}/c)^{-1}$")
+    ax.set_xlabel(r"$p_{\text{T}}$ (GeV/c)")
+    ax.legend(loc="upper right", frameon=False)
+
+    fig.tight_layout()
+    fig.savefig(output_dir / f"pt_distribution_{pt_hat_bin[0]}_{pt_hat_bin[1]}.pdf")
+    plt.close(fig)
+
+
+def angular_distribution_around_jet(jets: ak.Array, arrays: ak.Array, pt_hat_bin: Tuple[int, int], base_output_dir: Path) -> None:
+    # Setup
+    output_dir = base_output_dir
+    output_dir.mkdir(parents=True, exist_ok=True)
+    min_jet_pt = 20
+
+    # Particle selection
+    # Drop neutrinos.
+    arrays = arrays[(np.abs(arrays["particle_ID"]) != 12) & (np.abs(arrays["particle_ID"]) != 14) & (np.abs(arrays["particle_ID"]) != 16)]
+    all_status_codes = np.unique(ak.to_numpy(ak.flatten(arrays["status"])))
+
+    # Jets selection
+    # Keep jets with at least min_jet_pt GeV.
+    jets = jets[jets.pt > min_jet_pt]
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    hists = []
+    for status_code in all_status_codes:
+        # Select only particles of a particular status.
+        particle_mask = (arrays["status"] == status_code)
+
+        # Calculate all of the distances. Hopefully this doesn't run out of memory!
+        comb_jets, comb_particles = ak.unzip(ak.cartesian([jets, arrays[particle_mask]]))
+        distance_from_jet = np.sqrt((comb_jets.eta - comb_particles.eta) ** 2 + (comb_jets.phi - comb_particles.phi) ** 2)
+
+        # Create, fill, and plot the histogram
+        distance_hist = bh.Histogram(bh.axis.Regular(100, 0, 10), storage=bh.storage.Weight())
+        distance_hist.fill(ak.to_numpy(ak.flatten(distance_from_jet)))
+        hist = binned_data.BinnedData.from_existing_data(distance_hist)
+
+        # Normalize
+        # Bin widths
+        hist /= hist.axes[0].bin_widths
+        # N jets
+        hist /= ak.sum(ak.num(jets, axis=1))
+
+        ax.errorbar(
+            hist.axes[0].bin_centers,
+            hist.values,
+            xerr=hist.axes[0].bin_widths / 2,
+            label=f"Status = {status_code}",
+            marker="o",
+            linestyle="",
+        )
+
+        # Store for summary plot
+        hists.append(hist)
+
+    # Plot summary
+    h_all = sum(hists)
+    ax.errorbar(
+        h_all.axes[0].bin_centers,
+        h_all.values,
+        xerr=h_all.axes[0].bin_widths / 2,
+        label=f"Sum",
+        marker="o",
+        linestyle="",
+    )
+
+    # Label
+    ax.text(
+        0.03,
+        0.97,
+        "R = 0.4 " + r"anti-$k_{\text{T}}$ jets"
+        "\n" + r"$p_{\text{T}}^{\text{jet}} > " + fr"{min_jet_pt}\:\text{{GeV}}/c$"
+        "\n" + r"$\hat{p_{\text{T}}} =$ " + f"{pt_hat_bin[0]}-{pt_hat_bin[1]}",
+        transform=ax.transAxes,
+        horizontalalignment="left", verticalalignment="top", multialignment="left",
+    )
+    #ax.set_yscale("log")
+    ax.set_ylabel(r"$1/N_{\text{jets}} \text{d}N/\text{d}R$")
+    ax.set_xlabel(r"Distance from jet axis")
+    ax.legend(loc="upper right", frameon=False)
+
+    fig.tight_layout()
+    fig.savefig(output_dir / f"distance_from_jet_{pt_hat_bin[0]}_{pt_hat_bin[1]}.pdf")
+    plt.close(fig)
 
 if __name__ == "__main__":
-    input_arrays = ak.from_parquet("skim/events_per_chunk_1000/JetscapeHadronListBin100_110_00.parquet")
-    # We use some very different value to make it clear if something ever goes wrong.
-    # NOTE: It's important to do this before constructing anything else. Otherwise it can
-    #       mess up the awkward1 behaviors.
-    fill_none_value = -9999
-    input_arrays = ak.fill_none(input_arrays, fill_none_value)
+    for pt_hat_bin in [(7, 9), (20, 25), (50, 55), (100, 110), (250, 260), (500, 550), (900, 1000)]:
+        pt_hat_range = f"{pt_hat_bin[0]}_{pt_hat_bin[1]}"
+        print(f"Running for pt hat range: {pt_hat_range}")
+        events_per_chunk = 1000
+        filename = f"JetscapeHadronListBin{pt_hat_range}"
+        base_output_dir = Path("performance_studies")
+        input_filename = Path("skim") / f"events_per_chunk_{events_per_chunk}" / f"{filename}_00.parquet"
+        input_arrays = ak.from_parquet(input_filename)
+        # We use some very different value to make it clear if something ever goes wrong.
+        # NOTE: It's important to do this before constructing anything else. Otherwise it can
+        #       mess up the awkward1 behaviors.
+        fill_none_value = -9999
+        input_arrays = ak.fill_none(input_arrays, fill_none_value)
 
-    # Fully zip the arrays together.
-    arrays = ak.zip(dict(zip(ak.fields(input_arrays), ak.unzip(input_arrays))), depth_limit=None)
-    #arrays = ak.zip(
-    #    {
-    #        "particle_ID": input_arrays["particle_ID"],
-    #        "E": input_arrays["E"],
-    #        "px": input_arrays["px"],
-    #        "py": input_arrays["py"],
-    #        "pz": input_arrays["pz"],
-    #    },
-    #    depth_limit = None,
-    #)
-    #import IPython; IPython.embed()
+        # Fully zip the arrays together.
+        arrays = ak.zip(dict(zip(ak.fields(input_arrays), ak.unzip(input_arrays))), depth_limit=None)
+        #arrays = ak.zip(
+        #    {
+        #        "particle_ID": input_arrays["particle_ID"],
+        #        "E": input_arrays["E"],
+        #        "px": input_arrays["px"],
+        #        "py": input_arrays["py"],
+        #        "pz": input_arrays["pz"],
+        #    },
+        #    depth_limit = None,
+        #)
+        #import IPython; IPython.embed()
 
-    find_jets_arr(array=arrays)
+        particle_pt_by_status(arrays=arrays, pt_hat_bin=pt_hat_bin, base_output_dir=base_output_dir)
+        jets = find_jets_arr(array=arrays)
+        angular_distribution_around_jet(jets=jets, arrays=arrays, pt_hat_bin=pt_hat_bin, base_output_dir=base_output_dir)
