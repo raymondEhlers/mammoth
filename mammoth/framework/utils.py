5d06c554f5bb8fa52c8e23be88c4e61020d4cea2
Raymond Ehlers <raymond.ehlers@gmail.com>
2021-06-06 23:40:07 +0200

    Add group by for awkward arrays

--- mammoth/framework/utils.py
+++ mammoth/framework/utils.py
@@ -3,12 +3,46 @@
 .. codeauthor:: Raymond Ehlers <raymond.ehlers@cern.ch>, ORNL
 """
 
-from typing import Optional
+import collections.abc
+from typing import Optional, Sequence, Union
 
 import attr
+
+import awkward as ak
+import numpy as np
 
 
 @attr.s(frozen=True)
 class Range:
     min: Optional[float] = attr.ib()
     max: Optional[float] = attr.ib()
+
+
+def _groupby_lexsort(array: ak.Array, columns: Sequence[Union[str, int]]) -> ak.Array:
+    """Sort for groupby."""
+    sort = np.lexsort(tuple(np.asarray(array[:, col]) for col in reversed(columns)))
+    return array[sort]
+
+
+def group_by(array: ak.Array, by: Sequence[Union[str, int]]) -> ak.Array:
+    """ Group by for awkward arrays.
+
+    Args:
+        array: Array to be grouped. Must be convertable to numpy arrays.
+        by: Names or indices of columns to group by. The first column is the primary index for sorting,
+            second is secondary, etc.
+    Returns:
+        Array grouped by the columns.
+    """
+    # Validation
+    if not isinstance(by, collections.abc.Iterable):
+        by = [by]
+
+    # First, sort
+    sorted = _groupby_lexsort(array=array, columns=by)
+
+    # And then construct the array
+    return ak.unflatten(
+        sorted, ak.run_lengths(sorted[:, by[-1]])
+    )
+
---
+++ tests/test_utils.py
@@ -0,0 +1,106 @@
+"""Tests for utility functions.
+
+.. codeauthor:: Raymond Ehlers <raymond.ehlers@cern.ch>, ORNL
+"""
+
+from typing import Sequence
+
+import pytest
+
+import awkward as ak
+
+from mammoth.framework import utils
+
+@pytest.mark.parametrize(
+    "columns",
+    [
+        0,
+        [0],
+        [0, 1],
+    ], ids = ["first, int", "first, sequence", "first and second"])
+def test_group_by(columns: Sequence[int]) -> None:
+    array = ak.Array([
+        [   2.,    1.,    2.,    0.],
+        [   4.,    2.,    4.,    0.],
+        [   2.,    1.,  100.,    0.],
+        [   3.,    1.,    2.,    0.],
+        [   3.,    3.,    6.,    0.],
+        [   2.,    2.,  100.,    0.],
+        [   4.,    2.,    2.,    0.],
+        [   3.,    1.,    4.,    0.],
+        [   4.,    2.,    6.,    0.]
+    ])
+    group_by = utils.group_by(array=array, by=columns)
+
+    #desired_result = ak.Array([
+    #    [   2.,   1.,   2.,   0.],
+    #    [   3.,   1.,   2.,   0.],
+    #    [   4.,   1.,   2.,   0.],
+    #    [   2.,   2., 100.,   0.],
+    #    [   3.,   2.,   4.,   0.],
+    #    [   4.,   2.,   4.,   0.],
+    #    [   2.,   3., 100.,   0.],
+    #    [   3.,   3.,   6.,   0.],
+    #    [   4.,   3.,   6.,   0.],
+    #])
+
+    #desired_result = ak.Array([
+    #    [   2.,   1.,   2.,   0.],
+    #    [   2.,   1., 100.,   0.],
+    #    [   2.,   2., 100.,   0.],
+    #    [   3.,   1.,   2.,   0.],
+    #    [   3.,   1.,   4.,   0.],
+    #    [   3.,   3.,   6.,   0.],
+    #    [   4.,   2.,   2.,   0.],
+    #    [   4.,   2.,   4.,   0.],
+    #    [   4.,   2.,   6.,   0.],
+    #])
+
+    desired_result_group_by_first = ak.Array([
+        [
+            [   2.,   1.,   2.,   0.],
+            [   2.,   1., 100.,   0.],
+            [   2.,   2., 100.,   0.],
+        ],
+        [
+            [   3.,   1.,   2.,   0.],
+            [   3.,   3.,   6.,   0.],
+            [   3.,   1.,   4.,   0.],
+        ],
+        [
+            [   4.,   2.,   4.,   0.],
+            [   4.,   2.,   2.,   0.],
+            [   4.,   2.,   6.,   0.],
+        ],
+    ])
+
+    desired_result_group_by_first_and_second = ak.Array([
+        [
+            [   2.,   1.,   2.,   0.],
+            [   2.,   1., 100.,   0.],
+        ],
+        [
+            [   2.,   2., 100.,   0.],
+        ],
+        [
+            [   3.,   1.,   2.,   0.],
+            [   3.,   1.,   4.,   0.],
+        ], 
+        [
+            [   3.,   3.,   6.,   0.],
+        ],
+        [
+            [   4.,   2.,   4.,   0.],
+            [   4.,   2.,   2.,   0.],
+            [   4.,   2.,   6.,   0.],
+        ],
+    ])
+
+    comparison = desired_result_group_by_first
+    if not isinstance(columns, int) and len(columns) == 2:
+        comparison = desired_result_group_by_first_and_second
+
+    assert ak.all(
+        ak.flatten(group_by == comparison, axis=None)
+    )
+
