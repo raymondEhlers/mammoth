79ebd495747556481b22013ff3750aeb58be84d3
Raymond Ehlers <raymond.ehlers@gmail.com>
2021-06-14 22:36:57 +0200

    Some practical improvements

--- mammoth/framework/analysis/thermal_model.py
+++ mammoth/framework/analysis/thermal_model.py
@@ -8,13 +8,15 @@ from typing import Dict, Mapping, Optional, Sequence, Tuple
 
 import awkward as ak
 import numpy as np
+import numpy.typing as npt
 import vector
 
 from mammoth.framework import sources
 
 vector.register_awkward()
 
-def _transform_inputs(source_index_identifiers: Mapping[str, int], arrays: ak.Array, mass_hypothesis: float = 0.139, particle_columns: Optional[Sequence[str]] = None) -> ak.Array:
+def _transform_inputs(source_index_identifiers: Mapping[str, int], arrays: ak.Array, mass_hypothesis: float = 0.139, particle_columns: Optional[Mapping[str, npt.DTypeLike]] = None
+                      ) -> ak.Array:
     # Setup
     if not particle_columns:
         particle_columns = {"px": np.float32, "py": np.float32, "pz": np.float32, "E": np.float32, "index": np.int64}
@@ -25,6 +27,8 @@ def _transform_inputs(source_index_identifiers: Mapping[str, int], arrays: ak.Ar
     det_level = arrays["signal"]["det_level"]
     det_level["index"] = ak.local_index(det_level) + source_index_identifiers["signal"]
     det_level["m"] = det_level["pt"] * mass_hypothesis
+    # NOTE: This is fully equivalent because we registered vector:
+    #       >>> det_level = ak.with_name(det_level, name="Momentum4D")
     det_level = vector.Array(det_level)
     part_level = arrays["signal"]["part_level"]
     part_level["index"] = ak.local_index(part_level) + source_index_identifiers["signal"]
@@ -40,14 +44,19 @@ def _transform_inputs(source_index_identifiers: Mapping[str, int], arrays: ak.Ar
             "part_level": part_level,
             "det_level": det_level,
             # Practically, this is where we are performing the embedding
-            "hybrid": vector.Array(
-                ak.concatenate(
+            # Need to rezip so it applies the vector at the same level as the other collections
+            # (ie. we want `var * Momentum4D[...]`, but without the zip, we have `Momentum4D[var * ...]`)
+            # NOTE: For some reason, ak.concatenate returns float64 here. I'm not sure why, but for now
+            #       it's not diving into.
+            "hybrid": vector.zip(
+                dict(zip(particle_columns.keys(),
+                ak.unzip(ak.concatenate(
                     [
                         ak.Array({k: getattr(det_level, k) for k in particle_columns}),
                         ak.Array({k: getattr(background, k) for k in particle_columns}),
                     ], axis=1
-                )
-            ),
+                )))
+            )),
             # Include the rest of the non particle related fields (ie. event level info)
             **{k: v for k, v in zip(ak.fields(arrays["signal"]), ak.unzip(arrays["signal"])) if k not in ["det_level", "part_level"]},
         }
