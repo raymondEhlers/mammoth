1ea827cf0cd330a45c4b9b79f17b33e6846b5163
Raymond Ehlers <raymond.ehlers@gmail.com>
2021-08-17 11:00:30 -0700

    Minor bug fix

--- projects/eic/jobSteering/cades/run_job_with_singularity.sh
+++ projects/eic/jobSteering/cades/run_job_with_singularity.sh
@@ -5,4 +5,4 @@ module purge
 module load PE-gnu singularity/3.6.3
 
 export ECCE_ROOT="/lustre/or-scratch/cades-birthright/rehlers/ECCE"
-srun singularity exec -B ${ECCE_ROOT}/Singularity/cvmfs:/cvmfs -B /lustre/or-scratch:/lustre/or-scratch ${ECCE_ROOT}/Singularity/cvmfs/eic.opensciencegrid.org/singularity/rhic_sl7_ext.simg ${ECCE_ROOT}/jobSteering/run_Fun4All_G4_FullDetectorModular.sh "$@"
+singularity exec -B ${ECCE_ROOT}/Singularity/cvmfs:/cvmfs -B /lustre/or-scratch:/lustre/or-scratch ${ECCE_ROOT}/Singularity/cvmfs/eic.opensciencegrid.org/singularity/rhic_sl7_ext.simg ${ECCE_ROOT}/jobSteering/run_Fun4All_G4_FullDetectorModular.sh "$@"
