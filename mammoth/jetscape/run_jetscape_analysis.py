58c8bd5fc48e8f56200b07e0c9f02f891ea3c223
Raymond Ehlers <raymond.ehlers@gmail.com>
2021-12-12 23:08:46 +0100

    Cleanup some WIP files
    These types of settings are always going to change, so just commit them
    as I go

--- mammoth/jetscape/run_jetscape_analysis.py
+++ mammoth/jetscape/run_jetscape_analysis.py
@@ -213,7 +213,8 @@ def run() -> None:
         #"convert",
         "analyze_RAA",
     ]
-    jet_R_values = [0.2, 0.4, 0.5, 0.6]
+    jet_R_values = [0.2, 0.4, 0.5, 0.6, 0.8, 1.0]
+    #jet_R_values = [0.8, 1.0]
     #systems_to_process = _possible_systems[1:]
     systems_to_process = _possible_systems
     # Job execution configuration
@@ -257,7 +258,7 @@ def run() -> None:
                 setup_RAA_analysis(
                     system=system,
                     parquet_input_dir=_system_to_base_path[system] / "skim",
-                    read_jet_skim_from_file=False,
+                    read_jet_skim_from_file=True,
                     jet_R_values=jet_R_values,
                     min_jet_pt=5,
                     write_jets_to_tree=True,
