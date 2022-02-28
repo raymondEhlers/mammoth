81137636c68ea7b0f7ba1d4c5589d9d24dd85e91
Raymond Ehlers <raymond.ehlers@gmail.com>
2022-02-28 16:47:59 +0100

    Update typing for bindings

--- mammoth/_ext.pyi
+++ mammoth/_ext.pyi
@@ -3,10 +3,99 @@ from typing import ClassVar, List, Optional, Tuple, TypeVar, Union, overload
 import numpy as np
 import numpy.typing as npt
 
+DEFAULT_RAPIDITY_MAX: float
+
 class AreaSettings:
     area_type: str
     ghost_area: float
-    def __init__(self, area_type: str = ..., ghost_area: float = ...) -> None: ...
+    rapidity_max: float
+    def __init__(
+        self,
+        area_type: str = ...,
+        ghost_area: float = ...,
+        rapidity_max: float = ...,
+        repeat_N_ghosts: int = ...,
+        grid_scatter: float = ...,
+        kt_scatter: float = ...,
+        kt_mean: float = ...,
+        random_seed: List[int] = ...,
+    ) -> None: ...
+
+class JetFindingSettings:
+    R: float
+    def __init__(
+        self,
+        R: float,
+        algorithm: str,
+        recombination_scheme: str,
+        strategy: str = ...,
+        pt_range: Tuple[float, float] = ...,
+        eta_range: Tuple[float, float] = ...,
+        area_settings: Optional[AreaSettings] = ...,
+    ) -> None: ...
+
+class JetMedianBackgroundEstimator:
+    compute_rho_m: bool
+    constituent_pt_max: float
+    exclude_n_hardest_jets: int
+    use_area_four_vector: bool
+    def __init__(
+        self,
+        jet_finding_settings: JetFindingSettings,
+        compute_rho_m: bool = ...,
+        use_area_four_vector: bool = ...,
+        exclude_n_hardest_jets: int = ...,
+        constituent_pt_max: float = ...,
+    ) -> None: ...
+
+class GridMedianBackgroundEstimator:
+    grid_spacing: float
+    rapidity_max: float
+    def __init__(self, rapidity_max: float = ..., grid_spacing: float = ...) -> None: ...
+
+class BackgroundSubtractionType:
+    disabled: ClassVar[BackgroundSubtractionType] = ...
+    event_wise_constituent_subtraction: ClassVar[BackgroundSubtractionType] = ...
+    jet_wise_constituent_subtraction: ClassVar[BackgroundSubtractionType] = ...
+    rho: ClassVar[BackgroundSubtractionType] = ...
+    def __init__(self, value: int) -> None: ...
+    def __eq__(self, other: object) -> bool: ...
+    def __ge__(self, other: object) -> bool: ...
+    def __getstate__(self) -> int: ...
+    def __gt__(self, other: object) -> bool: ...
+    def __hash__(self) -> int: ...
+    def __index__(self) -> int: ...
+    def __int__(self) -> int: ...
+    def __le__(self, other: object) -> bool: ...
+    def __lt__(self, other: object) -> bool: ...
+    def __ne__(self, other: object) -> bool: ...
+    def __setstate__(self, state: int) -> None: ...
+    @property
+    def name(self) -> str: ...
+    @property
+    def value(self) -> int: ...
+
+class RhoSubtractor:
+    use_rho_M: bool
+    use_safe_mass: bool
+    def __init__(self, use_rho_M: bool = ..., use_safe_mass: bool = ...) -> None: ...
+
+class ConstituentSubtractor:
+    alpha: float
+    distance_measure: str
+    r_max: float
+    rapidity_max: float
+    def __init__(
+        self, r_max: float = ..., alpha: float = ..., rapidity_max: float = ..., distance_measure: str = ...
+    ) -> None: ...
+
+class BackgroundSubtraction:
+    def __init__(
+        self,
+        type: BackgroundSubtractionType,
+        estimator: Optional[Union[JetMedianBackgroundEstimator, GridMedianBackgroundEstimator]] = ...,
+        subtractor: Optional[Union[RhoSubtractor, ConstituentSubtractor]] = ...,
+    ) -> None: ...
 
 class ColumnarSplittings:
     def __init__(self, *args, **kwargs) -> None: ...  # type: ignore
@@ -43,31 +132,130 @@ class OutputWrapperDouble:
     @property
     def constituent_indices(self) -> List[List[int]]: ...
     @property
-    def jets(self) -> Tuple[npt.NDArray[np.float64],npt.NDArray[np.float64],npt.NDArray[np.float64],npt.NDArray[np.float64]]: ...
+    def jets(
+        self,
+    ) -> Tuple[npt.NDArray[np.float64], npt.NDArray[np.float64], npt.NDArray[np.float64], npt.NDArray[np.float64]]: ...
     @property
     def jets_area(self) -> npt.NDArray[np.float64]: ...
     @property
-    def subtracted_info(self) -> Optional[Tuple[Tuple[npt.NDArray[np.float64],npt.NDArray[np.float64],npt.NDArray[np.float64],npt.NDArray[np.float64]],npt.NDArray[np.int64]]]: ...
+    def subtracted_info(
+        self,
+    ) -> Optional[
+        Tuple[
+            Tuple[npt.NDArray[np.float64], npt.NDArray[np.float64], npt.NDArray[np.float64], npt.NDArray[np.float64]],
+            npt.NDArray[np.int64],
+        ]
+    ]: ...
 
 class OutputWrapperFloat:
     def __init__(self, *args, **kwargs) -> None: ...  # type: ignore
     @property
     def constituent_indices(self) -> List[List[int]]: ...
     @property
-    def jets(self) -> Tuple[npt.NDArray[np.float32],npt.NDArray[np.float32],npt.NDArray[np.float32],npt.NDArray[np.float32]]: ...
+    def jets(
+        self,
+    ) -> Tuple[npt.NDArray[np.float32], npt.NDArray[np.float32], npt.NDArray[np.float32], npt.NDArray[np.float32]]: ...
     @property
     def jets_area(self) -> npt.NDArray[np.float32]: ...
     @property
-    def subtracted_info(self) -> Optional[Tuple[Tuple[npt.NDArray[np.float32],npt.NDArray[np.float32],npt.NDArray[np.float32],npt.NDArray[np.float32]],npt.NDArray[np.int64]]]: ...
+    def subtracted_info(
+        self,
+    ) -> Optional[
+        Tuple[
+            Tuple[npt.NDArray[np.float32], npt.NDArray[np.float32], npt.NDArray[np.float32], npt.NDArray[np.float32]],
+            npt.NDArray[np.int64],
+        ]
+    ]: ...
 
 @overload
-def find_jets(px: npt.NDArray[np.float32], py: npt.NDArray[np.float32], pz: npt.NDArray[np.float32], E: npt.NDArray[np.float32], background_px: npt.NDArray[np.float32], background_py: npt.NDArray[np.float32], background_pz: npt.NDArray[np.float32], background_E: npt.NDArray[np.float32], jet_R: float, jet_algorithm: str, area_settings: AreaSettings, eta_range: Tuple[float,float] = ..., fiducial_acceptance: bool = ..., min_jet_pt: float = ..., background_subtraction: bool = ..., constituent_subtraction: Optional[ConstituentSubtractionSettings] = ...) -> OutputWrapperFloat: ...  # type: ignore
+def find_jets_new(  # type: ignore
+    px: npt.NDArray[np.float32],
+    py: npt.NDArray[np.float32],
+    pz: npt.NDArray[np.float32],
+    E: npt.NDArray[np.float32],
+    jet_finder_settings: JetFindingSettings,
+    background_px: npt.NDArray[np.float32],
+    background_py: npt.NDArray[np.float32],
+    background_pz: npt.NDArray[np.float32],
+    background_E: npt.NDArray[np.float32],
+    background_subtraction: BackgroundSubtraction,
+) -> OutputWrapperFloat: ...
 @overload
-def find_jets(px: npt.NDArray[np.float64], py: npt.NDArray[np.float64], pz: npt.NDArray[np.float64], E: npt.NDArray[np.float64], background_px: npt.NDArray[np.float64], background_py: npt.NDArray[np.float64], background_pz: npt.NDArray[np.float64], background_E: npt.NDArray[np.float64], jet_R: float, jet_algorithm: str, area_settings: AreaSettings, eta_range: Tuple[float,float] = ..., fiducial_acceptance: bool = ..., min_jet_pt: float = ..., background_subtraction: bool = ..., constituent_subtraction: Optional[ConstituentSubtractionSettings] = ...) -> OutputWrapperDouble: ...
+def find_jets_new(
+    px: npt.NDArray[np.float64],
+    py: npt.NDArray[np.float64],
+    pz: npt.NDArray[np.float64],
+    E: npt.NDArray[np.float64],
+    jet_finder_settings: JetFindingSettings,
+    background_px: npt.NDArray[np.float64],
+    background_py: npt.NDArray[np.float64],
+    background_pz: npt.NDArray[np.float64],
+    background_E: npt.NDArray[np.float64],
+    background_subtraction: BackgroundSubtraction,
+) -> OutputWrapperDouble: ...
 @overload
-def recluster_jet(px: npt.NDArray[np.float32], py: npt.NDArray[np.float32], pz: npt.NDArray[np.float32], E: npt.NDArray[np.float32], jet_R: float = ..., jet_algorithm: str = ..., area_settings: Optional[AreaSettings] = ..., eta_range: Tuple[float,float] = ..., store_recursive_splittings: bool = ...) -> JetSubstructureSplittings: ...
+def find_jets(px: npt.NDArray[np.float32], py: npt.NDArray[np.float32], pz: npt.NDArray[np.float32], E: npt.NDArray[np.float32], background_px: npt.NDArray[np.float32], background_py: npt.NDArray[np.float32], background_pz: npt.NDArray[np.float32], background_E: npt.NDArray[np.float32], jet_R: float, jet_algorithm: str, area_settings: AreaSettings, eta_range: Tuple[float, float] = ..., fiducial_acceptance: bool = ..., min_jet_pt: float = ..., background_subtraction: bool = ..., constituent_subtraction: Optional[ConstituentSubtractionSettings] = ...) -> OutputWrapperFloat: ...  # type: ignore
 @overload
-def recluster_jet(px: npt.NDArray[np.float64], py: npt.NDArray[np.float64], pz: npt.NDArray[np.float64], E: npt.NDArray[np.float64], jet_R: float = ..., jet_algorithm: str = ..., area_settings: Optional[AreaSettings] = ..., eta_range: Tuple[float,float] = ..., store_recursive_splittings: bool = ...) -> JetSubstructureSplittings: ...
+def find_jets(
+    px: npt.NDArray[np.float64],
+    py: npt.NDArray[np.float64],
+    pz: npt.NDArray[np.float64],
+    E: npt.NDArray[np.float64],
+    background_px: npt.NDArray[np.float64],
+    background_py: npt.NDArray[np.float64],
+    background_pz: npt.NDArray[np.float64],
+    background_E: npt.NDArray[np.float64],
+    jet_R: float,
+    jet_algorithm: str,
+    area_settings: AreaSettings,
+    eta_range: Tuple[float, float] = ...,
+    fiducial_acceptance: bool = ...,
+    min_jet_pt: float = ...,
+    background_subtraction: bool = ...,
+    constituent_subtraction: Optional[ConstituentSubtractionSettings] = ...,
+) -> OutputWrapperDouble: ...
+@overload
+def recluster_jet_new(
+    px: npt.NDArray[np.float32],
+    py: npt.NDArray[np.float32],
+    pz: npt.NDArray[np.float32],
+    E: npt.NDArray[np.float32],
+    jet_finding_settings: JetFindingSettings,
+    store_recursive_splittings: bool = ...,
+) -> JetSubstructureSplittings: ...
+@overload
+def recluster_jet_new(
+    px: npt.NDArray[np.float64],
+    py: npt.NDArray[np.float64],
+    pz: npt.NDArray[np.float64],
+    E: npt.NDArray[np.float64],
+    jet_finding_settings: JetFindingSettings,
+    store_recursive_splittings: bool = ...,
+) -> JetSubstructureSplittings: ...
+@overload
+def recluster_jet(
+    px: npt.NDArray[np.float32],
+    py: npt.NDArray[np.float32],
+    pz: npt.NDArray[np.float32],
+    E: npt.NDArray[np.float32],
+    jet_R: float = ...,
+    jet_algorithm: str = ...,
+    area_settings: Optional[AreaSettings] = ...,
+    eta_range: Tuple[float, float] = ...,
+    store_recursive_splittings: bool = ...,
+) -> JetSubstructureSplittings: ...
+@overload
+def recluster_jet(
+    px: npt.NDArray[np.float64],
+    py: npt.NDArray[np.float64],
+    pz: npt.NDArray[np.float64],
+    E: npt.NDArray[np.float64],
+    jet_R: float = ...,
+    jet_algorithm: str = ...,
+    area_settings: Optional[AreaSettings] = ...,
+    eta_range: Tuple[float, float] = ...,
+    store_recursive_splittings: bool = ...,
+) -> JetSubstructureSplittings: ...
 
 # ALICE
 # Fast sim
@@ -120,5 +308,10 @@ class TrackingEfficiencyPeriod:
     @property
     def value(self) -> int: ...
 
-def fast_sim_tracking_efficiency(track_pt: npt.NDArray[np.float64], track_eta: npt.NDArray[np.float64], event_activity: TrackingEfficiencyEventActivity, period: TrackingEfficiencyPeriod) -> object: ...
+def fast_sim_tracking_efficiency(
+    track_pt: npt.NDArray[np.float64],
+    track_eta: npt.NDArray[np.float64],
+    event_activity: TrackingEfficiencyEventActivity,
+    period: TrackingEfficiencyPeriod,
+) -> object: ...
 def find_event_activity(value: float) -> TrackingEfficiencyEventActivity: ...
