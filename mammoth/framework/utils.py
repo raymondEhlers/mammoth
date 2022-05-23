748020414042f089f226d3df884d0e54fcd13adb
Raymond Ehlers <raymond.ehlers@gmail.com>
2022-05-23 16:51:12 +0200

    Cleanup unused code
    I'm not not sure of the purpose of this function anymore. Better to
    remove it, as it will as also speed up loading since we don't require
    numba in the base utils anymore

--- mammoth/framework/utils.py
+++ mammoth/framework/utils.py
@@ -9,7 +9,6 @@ from typing import Optional, Sequence, Union
 import attr
 
 import awkward as ak
-import numba as nb
 import numpy as np
 
 
@@ -25,29 +24,11 @@ def _lexsort_for_groupby(array: ak.Array, columns: Sequence[Union[str, int]]) ->
     return array[sort]
 
 
-# @nb.njit
-def _run_lengths_for_multiple_columns(array: ak.Array, columns: Sequence[Union[str, int]]) -> nb.typed.List:
-    _previous_main_value = -1
-    _previous_secondary_value = -1
-    # previous_values = nb.typed.List()
-    run_lengths = nb.typed.List()
-    current_run_length = 0
-    for val in array:
-        if val[columns[0]] != _previous_main_value or val[columns[1]] != _previous_secondary_value:
-            _previous_main_value = val[columns[0]]
-            _previous_secondary_value = val[columns[1]]
-            run_lengths.append(current_run_length)
-            current_run_length = 0
-        current_run_length += 1
-
-    return run_lengths
-
-
 def group_by(array: ak.Array, by: Sequence[Union[str, int]]) -> ak.Array:
     """Group by for awkward arrays.
 
     Args:
-        array: Array to be grouped. Must be convertable to numpy arrays.
+        array: Array to be grouped. Must be convertible to numpy arrays.
         by: Names or indices of columns to group by. The first column is the primary index for sorting,
             second is secondary, etc.
     Returns:
@@ -68,8 +49,9 @@ def group_by(array: ak.Array, by: Sequence[Union[str, int]]) -> ak.Array:
     run_starts = [np.cumsum(np.asarray(l)) for l in run_lengths]  # noqa: E741
     # Combine all of the indices together into one array. Note that this isn't unique.
     combined = np.concatenate(run_starts)  # type: ignore
-    # TODO: Unique properly...
-    # See: https://stackoverflow.com/a/12427633/12907985
+    # NOTE: Unique can be done more efficient than using the naive implementation.
+    #       See: https://stackoverflow.com/a/12427633/12907985
+    #       However, it's not worth messing around with at the moment (May 2022).
     combined = np.unique(combined)  # type: ignore
 
     run_length = np.zeros(len(combined), dtype=np.int64)
