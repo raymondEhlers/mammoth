16fe5d155f54b45ba626703767b77ba9147f272d
Raymond Ehlers <raymond.ehlers@gmail.com>
2022-10-14 15:55:41 +0200

    Add file to grab references, inputs
    This way, a new setup can be created from scratch without having to
    track down a bunch of files. Ideally, we would put this in the git repo
    itself, but the files are too large

---
+++ tests/download_files_for_track_skim_validation.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+downloadInputFiles=$1
+
+inputFilesDate="2022-10-14"
+referenceFilesDate="2022-10-14"
+
+currentDir=$(realpath $(dirname "$0"))
+# input
+inputDir="${currentDir}/track_skim_validation/input"
+aliceInputTarGzFilename="alice_input_files-${inputFilesDate}.tar.gz"
+# reference
+referenceDir="${currentDir}/track_skim_validation/reference"
+referenceTarGzFilename="reference_files-${referenceFilesDate}.tar.gz"
+
+# Helpers
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+## Arbitrarily use the first file to check if the input is available.
+echo "=> ${bold}Input files${normal}"
+if [[ ! -z "${downloadInputFiles}" ]];
+then
+    if [[ ! -f ${inputDir}/alice/data/2017/LHC17p/000282343/pass1_FAST/AOD234/0001/root_archive.zip ]];
+    then
+        echo "--> Downloading missing ALICE input files"
+        pushd ${inputDir}
+        # NOTE: To get the download link, I had to share, open the share incognito, go to the details, and then copy the direct link
+        curl -O -J -L https://new.cernbox.cern.ch/remote.php/dav/public-files/PwTZazAvBDoWkYz/${aliceInputTarGzFilename}
+        # Extract the files in the inputDir
+        tar -xf ${aliceInputTarGzFilename}
+        # Cleanup
+        rm ${aliceInputTarGzFilename}
+        popd
+    else
+        echo "--> Already have ALICE input files - skipping"
+    fi
+else
+    echo "--> Skipping downloading ALICE input files. If you need them, please request them via passing an argument."
+fi
+
+echo -e "\n=> ${bold}Reference files${normal}"
+# This returns a string containing all of the files in the directory
+referenceDirFiles=$(shopt -s nullglob dotglob; echo ${referenceDir}/AnalysisResults*.root)
+# We want to count the number of files, so we split the string into an array
+referenceDirFiles=(${referenceDirFiles//;/ })
+# There should be 12 files: (pp, pythia, PbPb, embed_pythia, embed_pythia-pythia, embed_pythia-PbPb) x 2 for two jetR
+if [[ ! ${#referenceDirFiles[@]} -eq 12 ]];
+then
+    echo "--> Downloading missing reference files"
+    pushd ${referenceDir}
+    echo "current dir: $(PWD)"
+    # NOTE: To get the download link, I had to share, open the share incognito, go to the details, and then copy the direct link
+    curl -O -J -L https://new.cernbox.cern.ch/remote.php/dav/public-files/mMAUdHDFsWAuegk/${referenceTarGzFilename}
+    # Extract the files in the referenceDir
+    tar -xf ${referenceTarGzFilename}
+    # Cleanup
+    rm ${referenceTarGzFilename}
+    popd
+else
+    echo "--> Already have reference files - skipping!"
+fi
