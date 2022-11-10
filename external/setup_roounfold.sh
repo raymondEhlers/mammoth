c3dbe13667729bfb128820e62d26f473ac32307d
Raymond Ehlers <raymond.ehlers@gmail.com>
2022-11-10 15:35:37 +0100

    Fix typo in setup script

--- external/setup_roounfold.sh
+++ external/setup_roounfold.sh
@@ -3,5 +3,5 @@
 currentDir=$(realpath $(dirname "$0"))
 
 export ROOUNFOLD_ROOT="$currentDir/install/roounfold"
-export PATH="${ROOTUNFOLD_ROOT}/bin:${PATH}"
-export LD_LIBRARY_PATH="${ROOTUNFOLD_ROOT}/lib:${LD_LIBRARY_PATH}"
+export PATH="${ROOUNFOLD_ROOT}/bin:${PATH}"
+export LD_LIBRARY_PATH="${ROOUNFOLD_ROOT}/lib:${LD_LIBRARY_PATH}"
