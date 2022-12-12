66fe7b8df2f4859e3e5e9b5d882fcd07103ab349
Raymond Ehlers <raymond.ehlers@gmail.com>
2022-12-12 14:08:00 +0100

    Helper to load conda env in jobs without needing shell initialization
    We can't use the standard methods because it relies on helpers in the
    shell initialization, which we won't have available

---
+++ external/load_conda.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+envName=${1}
+
+# Need to setup conda first in the subshell
+# See https://github.com/conda/conda/issues/7980
+MY_CONDA_BASE=$(conda info --base)
+source $MY_CONDA_BASE/etc/profile.d/conda.sh
+
+# And then load the environment
+conda activate ${envName}
