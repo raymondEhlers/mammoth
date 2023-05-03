672369a16f3d0f3c3f9fdbb88d54c18cb228689d
Raymond Ehlers <raymond.ehlers@gmail.com>
2023-05-03 14:55:53 -0700

    Add convenience for printing

--- src/mammoth/framework/task.py
+++ src/mammoth/framework/task.py
@@ -19,3 +19,14 @@ class Output:
     collision_system: str
     hists: dict[str, hist.Hist] = attrs.field(factory=dict)
     results: dict[str, Any] = attrs.field(factory=dict)
+
+    def print(self) -> None:
+        """Print the message to the logger.
+
+        Note:
+            This is implemented as a convenience function since it otherwise
+            won't evaluate newlines in the message. We don't implement __str__
+            or __repr__ since those require an additional function call (ie. explicit print).
+            This is all just for convenience.
+        """
+        logger.info(self.message)
