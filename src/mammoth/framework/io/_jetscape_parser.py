700b0e654110f3adac7dd0580ba0d9cf5f0267e2
Raymond Ehlers <raymond.ehlers@gmail.com>
2023-05-19 13:38:39 -0700

    Support updating the chunk size in the parser via generator

--- src/mammoth/framework/io/_jetscape_parser.py
+++ src/mammoth/framework/io/_jetscape_parser.py
@@ -10,7 +10,7 @@ import logging
 import os
 import typing
 from pathlib import Path
-from typing import Any, Callable, Iterator
+from typing import Any, Callable, Generator, Iterator
 
 import attrs
 import awkward as ak
@@ -470,7 +470,7 @@ DEFAULT_EVENTS_PER_CHUNK_SIZE = int(1e5)
 
 def read_events_in_chunks(
     filename: Path, events_per_chunk: int = DEFAULT_EVENTS_PER_CHUNK_SIZE
-) -> Iterator[ChunkGenerator]:
+) -> Generator[ChunkGenerator, int | None, None]:
     """Read events in chunks from stored JETSCAPE FinalState* ASCII files.
 
     This provides access to the lines of the file itself, but it is up to the user to parse each line.
@@ -516,16 +516,21 @@ def read_events_in_chunks(
 
         # Now, need to setup chunks.
         # NOTE: The headers and additional info are passed through the ChunkGenerator.
+        requested_chunk_size: int | None = events_per_chunk
         while True:
+            # This check is only needed for subsequent iterations - not the first
+            if requested_chunk_size is None:
+                requested_chunk_size = events_per_chunk
+
             # We keep an explicit reference to the chunk so we can set the end of file state
             # if we reached the end of the file.
             chunk = ChunkGenerator(
                 g=read_lines,
-                events_per_chunk=events_per_chunk,
+                events_per_chunk=requested_chunk_size,
                 cross_section=cross_section,
                 file_format_version=file_format_version,
             )
-            yield chunk
+            requested_chunk_size = yield chunk
             if chunk.reached_end_of_file:
                 break
 
@@ -628,7 +633,7 @@ def _parse_with_numpy(chunk_generator: Iterator[str]) -> npt.NDArray[Any]:
     return np.loadtxt(chunk_generator)
 
 
-def read(filename: Path | str, events_per_chunk: int, parser: str = "pandas") -> Iterator[ak.Array]:
+def read(filename: Path | str, events_per_chunk: int, parser: str = "pandas") -> Generator[ak.Array, int | None, None]:
     """Read a JETSCAPE FinalState{Hadrons,Partons} ASCII output file in chunks.
 
     This is the primary user function. We read in chunks to keep the memory usage manageable.
@@ -656,99 +661,109 @@ def read(filename: Path | str, events_per_chunk: int, parser: str = "pandas") ->
     parsing_function = parsing_function_map[parser]
 
     # Read the file, creating chunks of events.
-    for i, chunk_generator in enumerate(read_events_in_chunks(filename=filename, events_per_chunk=events_per_chunk)):
-        # Give a notification just in case the parsing is slow...
-        logger.debug(f"New chunk {i}")
+    event_generator = read_events_in_chunks(filename=filename, events_per_chunk=events_per_chunk)
+    try:
+        i = 0
+        chunk_generator = next(event_generator)
+        while True:
+            # Give a notification just in case the parsing is slow...
+            logger.debug(f"New chunk {i}")
 
-        # First, parse the lines. We need to make this call before attempt to convert into events because the necessary
-        # info (namely, n particles per event) is only available and valid after we've parse the lines.
-        res = parsing_function(iter(chunk_generator))
+            # First, parse the lines. We need to make this call before attempt to convert into events because the necessary
+            # info (namely, n particles per event) is only available and valid after we've parse the lines.
+            res = parsing_function(iter(chunk_generator))
 
-        # Before we do anything else, if our events_per_chunk is a even divisor of the total number of events
-        # and we've hit the end of the file, we can return an empty generator after trying to parse the chunk.
-        # In that case, we're done - just break.
-        # NOTE: In the case where we've reached the end of file, but we've parsed some events, we want to continue
-        #       on so we don't lose any events.
-        if chunk_generator.reached_end_of_file and len(chunk_generator.headers) == 0:
-            break
+            # Before we do anything else, if our events_per_chunk is a even divisor of the total number of events
+            # and we've hit the end of the file, we can return an empty generator after trying to parse the chunk.
+            # In that case, we're done - just break.
+            # NOTE: In the case where we've reached the end of file, but we've parsed some events, we want to continue
+            #       on so we don't lose any events.
+            if chunk_generator.reached_end_of_file and len(chunk_generator.headers) == 0:
+                break
 
-        # Now, convert into the awkward array structure.
-        # logger.info(f"n_particles_per_event len: {len(chunk_generator.n_particles_per_event())}, array: {chunk_generator.n_particles_per_event()}")
-        array_with_events = ak.unflatten(ak.Array(res), chunk_generator.n_particles_per_event())
+            # Now, convert into the awkward array structure.
+            # logger.info(f"n_particles_per_event len: {len(chunk_generator.n_particles_per_event())}, array: {chunk_generator.n_particles_per_event()}")
+            array_with_events = ak.unflatten(ak.Array(res), chunk_generator.n_particles_per_event())
 
-        # Cross checks.
-        # Length check that we have as many events as expected based on the number of headers.
-        # logger.debug(f"ak.num: {ak.num(array_with_events, axis = 0)}, len headers: {len(chunk_generator.headers)}")
-        assert ak.num(array_with_events, axis=0) == len(chunk_generator.headers)
-        # Check that n particles agree
-        n_particles_from_header = np.array([header.n_particles for header in chunk_generator.headers])
-        # logger.info(f"n_particles from headers: {n_particles_from_header}")
-        # logger.info(f"n_particles from array: {ak.num(array_with_events, axis = 1)}")
-        assert (np.asarray(ak.num(array_with_events, axis=1)) == n_particles_from_header).all()
-        # State of the chunk
-        # logger.debug(f"Reached end of file: {chunk_generator.reached_end_of_file}")
-        # logger.debug(f"Incomplete chunk: {chunk_generator.incomplete_chunk}")
-        # Let the use know so they're not surprised.
-        if chunk_generator.incomplete_chunk:
-            logger.warning(
-                f"Requested {chunk_generator.events_per_chunk} events, but only {chunk_generator.events_contained_in_chunk} are available because we hit the end of the file."
+            # Cross checks.
+            # Length check that we have as many events as expected based on the number of headers.
+            # logger.debug(f"ak.num: {ak.num(array_with_events, axis = 0)}, len headers: {len(chunk_generator.headers)}")
+            assert ak.num(array_with_events, axis=0) == len(chunk_generator.headers)
+            # Check that n particles agree
+            n_particles_from_header = np.array([header.n_particles for header in chunk_generator.headers])
+            # logger.info(f"n_particles from headers: {n_particles_from_header}")
+            # logger.info(f"n_particles from array: {ak.num(array_with_events, axis = 1)}")
+            assert (np.asarray(ak.num(array_with_events, axis=1)) == n_particles_from_header).all()
+            # State of the chunk
+            # logger.debug(f"Reached end of file: {chunk_generator.reached_end_of_file}")
+            # logger.debug(f"Incomplete chunk: {chunk_generator.incomplete_chunk}")
+            # Let the use know so they're not surprised.
+            if chunk_generator.incomplete_chunk:
+                logger.warning(
+                    f"Requested {chunk_generator.events_per_chunk} events, but only {chunk_generator.events_contained_in_chunk} are available because we hit the end of the file."
+                )
+
+            # Header info
+            header_level_info = {
+                "event_plane_angle": np.array([header.event_plane_angle for header in chunk_generator.headers], np.float32),
+                "event_ID": np.array([header.event_number for header in chunk_generator.headers], np.uint16),
+            }
+            if chunk_generator.headers[0].event_weight > -1:
+                header_level_info["event_weight"] = np.array([header.event_weight for header in chunk_generator.headers], np.float32)
+            if chunk_generator.headers[0].pt_hat > -1:
+                header_level_info["pt_hat"] = np.array([header.pt_hat for header in chunk_generator.headers], np.float32)
+            if chunk_generator.headers[0].vertex_x > -999:
+                header_level_info["vertex_x"] = np.array([header.vertex_x for header in chunk_generator.headers], np.float32)
+            if chunk_generator.headers[0].vertex_y > -999:
+                header_level_info["vertex_y"] = np.array([header.vertex_y for header in chunk_generator.headers], np.float32)
+            if chunk_generator.headers[0].vertex_z > -999:
+                header_level_info["vertex_z"] = np.array([header.vertex_z for header in chunk_generator.headers], np.float32)
+
+            # Cross section info
+            if chunk_generator.cross_section:
+                # Even though this is a dataset level quantity, we need to match the structure in order to zip them together for storage.
+                # Since we're repeating the value, hopefully this will be compressed effectively.
+                header_level_info["cross_section"] = np.full_like(
+                    header_level_info["event_plane_angle"], chunk_generator.cross_section.value
+                )
+                header_level_info["cross_section_error"] = np.full_like(
+                    header_level_info["event_plane_angle"], chunk_generator.cross_section.error
+                )
+
+            # Assemble all of the information in a single awkward array and pass it on.
+            _result = yield ak.zip(
+                {
+                    # Header level info
+                    **header_level_info,
+                    # Particle level info
+                    # As I've learned from experience, it's much more convenient to store the particles in separate columns.
+                    # Trying to fit them in alongside the event level info makes life far more difficult.
+                    "particles": ak.zip(
+                        {
+                            "particle_ID": ak.values_astype(array_with_events[:, :, 1], np.int32),
+                            # We're only considering final state hadrons or partons, so status codes are limited to a few values.
+                            # -1 are holes, while >= 0 are signal particles (includes both the jet signal and the recoils).
+                            # So we can't differentiate the recoil from the signal.
+                            "status": ak.values_astype(array_with_events[:, :, 2], np.int8),
+                            "E": ak.values_astype(array_with_events[:, :, 3], np.float32),
+                            "px": ak.values_astype(array_with_events[:, :, 4], np.float32),
+                            "py": ak.values_astype(array_with_events[:, :, 5], np.float32),
+                            "pz": ak.values_astype(array_with_events[:, :, 6], np.float32),
+                            # We could skip eta and phi since we can always recalculate them. However, since we've already parsed
+                            # them, we may as well pass them along.
+                            "eta": ak.values_astype(array_with_events[:, :, 7], np.float32),
+                            "phi": ak.values_astype(array_with_events[:, :, 8], np.float32),
+                        }
+                    ),
+                },
+                depth_limit=1,
             )
 
-        # Header info
-        header_level_info = {
-            "event_plane_angle": np.array([header.event_plane_angle for header in chunk_generator.headers], np.float32),
-            "event_ID": np.array([header.event_number for header in chunk_generator.headers], np.uint16),
-        }
-        if chunk_generator.headers[0].event_weight > -1:
-            header_level_info["event_weight"] = np.array([header.event_weight for header in chunk_generator.headers], np.float32)
-        if chunk_generator.headers[0].pt_hat > -1:
-            header_level_info["pt_hat"] = np.array([header.pt_hat for header in chunk_generator.headers], np.float32)
-        if chunk_generator.headers[0].vertex_x > -999:
-            header_level_info["vertex_x"] = np.array([header.vertex_x for header in chunk_generator.headers], np.float32)
-        if chunk_generator.headers[0].vertex_y > -999:
-            header_level_info["vertex_y"] = np.array([header.vertex_y for header in chunk_generator.headers], np.float32)
-        if chunk_generator.headers[0].vertex_z > -999:
-            header_level_info["vertex_z"] = np.array([header.vertex_z for header in chunk_generator.headers], np.float32)
-
-        # Cross section info
-        if chunk_generator.cross_section:
-            # Even though this is a dataset level quantity, we need to match the structure in order to zip them together for storage.
-            # Since we're repeating the value, hopefully this will be compressed effectively.
-            header_level_info["cross_section"] = np.full_like(
-                header_level_info["event_plane_angle"], chunk_generator.cross_section.value
-            )
-            header_level_info["cross_section_error"] = np.full_like(
-                header_level_info["event_plane_angle"], chunk_generator.cross_section.error
-            )
-
-        # Assemble all of the information in a single awkward array and pass it on.
-        yield ak.zip(
-            {
-                # Header level info
-                **header_level_info,
-                # Particle level info
-                # As I've learned from experience, it's much more convenient to store the particles in separate columns.
-                # Trying to fit them in alongside the event level info makes life far more difficult.
-                "particles": ak.zip(
-                    {
-                        "particle_ID": ak.values_astype(array_with_events[:, :, 1], np.int32),
-                        # We're only considering final state hadrons or partons, so status codes are limited to a few values.
-                        # -1 are holes, while >= 0 are signal particles (includes both the jet signal and the recoils).
-                        # So we can't differentiate the recoil from the signal.
-                        "status": ak.values_astype(array_with_events[:, :, 2], np.int8),
-                        "E": ak.values_astype(array_with_events[:, :, 3], np.float32),
-                        "px": ak.values_astype(array_with_events[:, :, 4], np.float32),
-                        "py": ak.values_astype(array_with_events[:, :, 5], np.float32),
-                        "pz": ak.values_astype(array_with_events[:, :, 6], np.float32),
-                        # We could skip eta and phi since we can always recalculate them. However, since we've already parsed
-                        # them, we may as well pass them along.
-                        "eta": ak.values_astype(array_with_events[:, :, 7], np.float32),
-                        "phi": ak.values_astype(array_with_events[:, :, 8], np.float32),
-                    }
-                ),
-            },
-            depth_limit=1,
-        )
+            # Update for next step
+            chunk_generator = event_generator.send(_result)
+            i += 1
+    except StopIteration:
+        pass
 
 
 def full_events_to_only_necessary_columns_E_px_py_pz(arrays: ak.Array) -> ak.Array:
