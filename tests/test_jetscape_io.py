b1c6eadce432de0bbb0ef8cfd1aa22e10a591692
Raymond Ehlers <raymond.ehlers@gmail.com>
2023-05-19 17:24:17 -0700

    Add checks for changing chunk size, transforming legacy skims

--- tests/test_jetscape_io.py
+++ tests/test_jetscape_io.py
@@ -5,11 +5,14 @@
 
 import logging
 from pathlib import Path
+from typing import Any
 
 import awkward as ak
+import numpy as np
 import pytest
 
 from mammoth.framework.io import _jetscape_parser
+from mammoth.framework.io import jetscape
 
 
 logger = logging.getLogger(__name__)
@@ -75,7 +78,10 @@ def array_looks_good(reference_arrays: ak.Array, arrays: ak.Array, header_versio
         5, 16, 50, 5000,
     ], ids=["Multiple, divisible: 5", "Multiple, indivisible: 16", "Equal: 50", "Larger: 5000"]
 )
-def test_parsing(header_version: int, events_per_chunk: int) -> None:
+def test_parsing(caplog: Any, header_version: int, events_per_chunk: int) -> None:
+    # Setup
+    caplog.set_level(logging.INFO)
+
     input_filename = here / "jetscape_parser" / f"final_state_hadrons_header_v{header_version}.dat"
     if not input_filename.exists():
         raise ValueError("Missing input files - please download the files with the script")
@@ -105,8 +111,11 @@ def test_parsing(header_version: int, events_per_chunk: int) -> None:
         5, 16, 50, 5000,
     ], ids=["Multiple, divisible: 5", "Multiple, indivisible: 16", "Equal: 50", "Larger: 5000"]
 )
-def test_parsing_with_parquet(header_version: int, events_per_chunk: int, tmp_path: Path) -> None:
+def test_parsing_with_parquet(caplog: Any, header_version: int, events_per_chunk: int, tmp_path: Path) -> None:
     """Parse to parquet, read back, and compare."""
+    # Setup
+    caplog.set_level(logging.INFO)
+
     input_filename = here / "jetscape_parser" / f"final_state_hadrons_header_v{header_version}.dat"
     if not input_filename.exists():
         raise ValueError("Missing input files - please download the files with the script")
@@ -135,3 +144,137 @@ def test_parsing_with_parquet(header_version: int, events_per_chunk: int, tmp_pa
         )
 
         assert array_looks_good(reference_arrays=reference_arrays, arrays=arrays, header_version=header_version,)
+
+@pytest.mark.parametrize(
+    "events_per_chunk",
+    [
+        [5, 5, 5, 5, 10, 10, 10],
+        [5, 5, 5, 5, 10, 10, 10, 5],
+        [5, 5, 5, 5, 10, 10, 9, 5],
+    ], ids=["Fits into input data", "Fits into input data, but asks for extra", "Doesn't fit into input data"]
+)
+def test_parsing_with_changing_chunk_size(caplog: Any, events_per_chunk: list[int]) -> None:
+    # Setup
+    caplog.set_level(logging.INFO)
+    header_version = 2
+
+    # Retrieve input
+    N_EVENTS_IN_FILE = 50
+    input_filename = here / "jetscape_parser" / f"final_state_hadrons_header_v{header_version}.dat"
+    if not input_filename.exists():
+        raise ValueError("Missing input files - please download the files with the script")
+
+    event_generator = _jetscape_parser.read(filename=input_filename, events_per_chunk=events_per_chunk[0], parser="pandas")
+    expected_length_iter = iter(events_per_chunk)
+    # NOTE: The length of the file is 50 events, so if the length ever matches 50, we would expect it to be done.
+    cumulative_sum = np.cumsum(events_per_chunk)
+    expect_agreement = np.any([v for v in cumulative_sum if v == N_EVENTS_IN_FILE])
+    expected_disagreement_index = np.where(cumulative_sum > N_EVENTS_IN_FILE)[0]
+    if len(expected_disagreement_index) > 0:
+        expected_disagreement_index = expected_disagreement_index[0]
+    i = 0
+    try:
+        arrays = next(event_generator)
+        current_expected_length = next(expected_length_iter)
+        while True:
+            print(f"{current_expected_length=}")
+            if expect_agreement or expected_disagreement_index != i:
+                assert len(arrays) == current_expected_length
+            else:
+                with pytest.raises(AssertionError):
+                    assert len(arrays) == current_expected_length
+
+            # Update for next round
+            # NOTE: If the iterator is exhausted, it will stop here
+            current_expected_length = next(expected_length_iter)
+            arrays = event_generator.send(current_expected_length)
+            i += 1
+    except StopIteration:
+        ...
+
+    expect_exhaustion_of_lengths = (cumulative_sum[-1] == N_EVENTS_IN_FILE)
+    if expect_exhaustion_of_lengths:
+        assert i == (len(events_per_chunk) - 1)
+
+
+@pytest.mark.parametrize(
+    "legacy_skim", [False, True]
+)
+def test_jetscape_file_source_parquet(caplog: Any, legacy_skim: bool, tmp_path: Path) -> None:
+    """ Parse handling parquet inputs, including legacy skims. """
+    # Setup
+    caplog.set_level(logging.INFO)
+    header_version = 2
+
+    # Retrieve input
+    N_EVENTS_IN_FILE = 50
+    input_filename = here / "jetscape_parser" / f"final_state_hadrons_header_v{header_version}.dat"
+    if not input_filename.exists():
+        raise ValueError("Missing input files - please download the files with the script")
+    events_per_chunk = [N_EVENTS_IN_FILE]
+
+    # Need to recreate a legacy skim...
+    base_output_filename = tmp_path / "test.parquet"
+    if legacy_skim:
+        # If we go back to the original parser, it will give the legacy skim format.
+        _jetscape_parser.parse_to_parquet(
+            base_output_filename=base_output_filename,
+            store_only_necessary_columns=True,
+            input_filename=input_filename,
+            events_per_chunk=events_per_chunk[0],
+        )
+        # NOTE: The filename was changed because we passed some value for events_per_chunk, so we need to find
+        #       the new filename.
+        output_filenames = list(tmp_path.glob("*.parquet"))
+        if len(output_filenames) != 1:
+            raise ValueError(f"Found wrong number of output filenames: {output_filenames}")
+        base_output_filename = output_filenames[0]
+    else:
+        # This is a bit of a roundtrip test, but good enough for now. This will transform the output as expected,
+        # and then we can read it back in.
+        jetscape_source = jetscape.FileSource(filename=input_filename, metadata={"legacy_skim": legacy_skim})
+        arrays = next(jetscape_source.gen_data(chunk_size=events_per_chunk[0]))
+        ak.to_parquet(
+            array=arrays,
+            destination=str(base_output_filename),
+            compression="zstd",
+            parquet_dictionary_encoding=True,
+            parquet_byte_stream_split=True,
+        )
+
+    # And make sure we process this file...
+    # There's only one chunk, so we can handle it with one filename.
+    input_filename = base_output_filename
+
+    jetscape_source = jetscape.FileSource(filename=input_filename, metadata={"legacy_skim": legacy_skim})
+    event_generator = jetscape_source.gen_data(chunk_size=events_per_chunk[0])
+    expected_length_iter = iter(events_per_chunk)
+    # NOTE: The length of the file is 50 events, so if the length ever matches 50, we would expect it to be done.
+    cumulative_sum = np.cumsum(events_per_chunk)
+    expect_agreement = np.any([v for v in cumulative_sum if v == N_EVENTS_IN_FILE])
+    expected_disagreement_index = np.where(cumulative_sum > N_EVENTS_IN_FILE)[0]
+    if len(expected_disagreement_index) > 0:
+        expected_disagreement_index = expected_disagreement_index[0]
+    i = 0
+    try:
+        arrays = next(event_generator)
+        current_expected_length = next(expected_length_iter)
+        while True:
+            print(f"{current_expected_length=}")
+            if expect_agreement or expected_disagreement_index != i:
+                assert len(arrays) == current_expected_length
+            else:
+                with pytest.raises(AssertionError):
+                    assert len(arrays) == current_expected_length
+
+            # Update for next round
+            # NOTE: If the iterator is exhausted, it will stop here
+            current_expected_length = next(expected_length_iter)
+            arrays = event_generator.send(current_expected_length)
+            i += 1
+    except StopIteration:
+        ...
+
+    expect_exhaustion_of_lengths = (cumulative_sum[-1] == N_EVENTS_IN_FILE)
+    if expect_exhaustion_of_lengths:
+        assert i == (len(events_per_chunk) - 1)
