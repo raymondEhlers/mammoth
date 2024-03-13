3a03a1b2ecae8bed852077c5ae3da1d0cd80a9e7
Raymond Ehlers <raymond.ehlers@gmail.com>
2024-03-12 23:54:32 -0700

    Undo "fix" for jupyter + rich - it didn't help

--- src/mammoth/helpers.py
+++ src/mammoth/helpers.py
@@ -19,11 +19,7 @@ logger = logging.getLogger(__name__)
 
 # We need a consistent console object to set everything up properly
 # (Namely, logging with the progress bars), so we define it here.
-# NOTE: The `force_jupyter=False`` argument is to try to fix logging in vscode.
-#       It doesn't show up properly, as it stands (extra spaces, odd line lengths,
-#       poor colors on dark backgrounds). However, I'm unsure if this will actually
-#       resolve it (Feb 2024)
-rich_console = Console(force_jupyter=False)
+rich_console = Console()
 
 
 @attrs.define
